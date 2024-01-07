1b2c0e82179d6454e575d4ba4f8abbb7bc219c52
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-01-07 19:46:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
 from time import sleep
+
+
+def input_slice(string):
+    list1 = []
+    list1[:0] = string
+    return list1
 
 
 def ip_to_bin():
@@ -7,12 +12,26 @@ def ip_to_bin():
     ip = input("\nIP to convert: ")
     sleep(1)
 
-    binary = " ".join(format(ord(char), "b") for char in ip)
-    sleep(1)
+    if ip == "99":
+        print("Exiting!")
+        exit()
 
-    enc_out = f"\nIp          : {ip} \n\nBinary Code : {binary}"
+    elif not 7 <= len(ip) <= 15:
+        print("\nThis IP is Invalid")
 
-    print(enc_out)
+    else:
+        for _ in input_slice(ip):
+            if not _ in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+                print("\nThis is not an IP")
+                break
+            else:
+                binary = " ".join(format(ord(char), "b") for char in ip)
+                sleep(1)
+
+                enc_out = f"\nIp          : {ip} \n\nBinary Code : {binary}"
+
+                print(enc_out)
+                break
 
 
 def bin_to_ip():
@@ -20,27 +39,49 @@ def bin_to_ip():
     bin_code = input("\nBinary Code to convert: ")
     sleep(1)
 
-    ip = "".join(chr(int(k, 2)) for k in bin_code.split(" "))
+    if bin_code == "99":
+        print("Exiting!")
+        exit()
 
-    sleep(1.5)
+    elif not 48 <= len(bin_code) <= 109:
+        print("\nThis is for an invalid ip")
 
-    dec_out = f"\nBinary Code : {bin_code} \n\nIp          : {ip}"
+    elif bin_code == "110010 110101 110101 101110 110010 110101 110101 101110 110010 110101 110101 101110 110010 110101 110101":
+        print(f"\nBinary Code : {bin_code} \n\nIp          : 255.255.255.255 (Broadcast IP)")
+        
+    else:
+        for _ in input_slice(bin_code):
+            if not _ in ["1", "0", " "]:
+                print("\nThis is not a Binary Code")
+                break
+            else:
+                ip = "".join(chr(int(k, 2)) for k in bin_code.split(" "))
 
-    print(dec_out)
+                sleep(1.5)
+
+                dec_out = f"\nBinary Code : {bin_code} \n\nIp          : {ip}"
+
+                print(dec_out)
+                break
 
 
 while True:
     try:
-        print("\n1 - Ip to Binary  |  2 - Binary to IPv4/v6 |  Q - Quit")
-        get_input = input("\nWhat Do You Want? : ")
+        mode = input('''
+|-PRESS-----------------|
+| 1 - Ip to Binary      |
+| 2 - Binary to IPv4/v6 |
+| 99 - Quit             |
+|-----------------------|
+|--> ''')
 
-        if get_input == "1":
+        if mode == "1":
             ip_to_bin()
 
-        elif get_input == "2":
+        elif mode == "2":
             bin_to_ip()
 
-        elif get_input == "q" or "Q":
+        elif mode == "99":
             print("Exiting!")
             exit()
 
